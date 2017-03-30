2e58a04526171fba7645add3164632bdb175dbda
Annyv <annybella18@gmail.com>
2017-03-30 10:59:54 -0400

    sequelized complete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,6 @@ var express = require("express");
 var models  = require('../models');
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
-
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   models.Burger.findAll({}).then(function(data) {
@@ -17,8 +14,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-  burger.create(["burger_name", "devoured"], 
-    [req.body.burgerName, false], function() {
+  models.Burger.create({
+    burger_name: req.body.burgerName,
+    devoured: false 
+    }).then(function() {
     res.redirect("/");
   });
 });
@@ -26,24 +25,16 @@ router.post("/", function(req, res) {
 router.put("/:id", function(req, res) {
 
   models.Burger.update({
-
     devoured: true
-
   },
-
   {
-
     where: { id : req.params.id }
-
   }
   ).then(function() {
-
      res.redirect("/");
   });
 
 });
-
-
 
 // Export routes for server.js to use.
 module.exports = router;
